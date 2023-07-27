9148aa117c5ea9016ec68b47fd390df29def5ee5
seattlenomnom <mark.crapser@gmail.com>
2023-07-26 22:45:40 -0700

    initial commit

---
+++ makefile
@@ -0,0 +1,4 @@
+# makefile for ptr11p4
+#
+ptr11p4: ptr11p4.c
+	gcc -g ptr11p4.c
---
+++ ptr11p4.c
@@ -0,0 +1,72 @@
+/* Source file for ptr11p4
+ *
+ * create a singly linked list with a list head. Create a function to delete
+ * a given entry.
+ *
+*/
+
+
+/* includes */
+
+
+
+
+
+
+
+
+
+
+/* defines */
+
+
+
+
+
+/* define structures */
+
+
+
+
+
+
+
+
+/* function declarations */
+
+
+
+
+
+int main(int argc, char *argv[]) {
+
+
+
+
+
+
+
+    return(0);
+}
+
+
+
+/* function definitions */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
