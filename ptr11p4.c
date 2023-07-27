a2d815755773207d5847eb5198a41c1b6911a2bc
seattlenomnom <mark.crapser@gmail.com>
2023-07-26 23:10:36 -0700

    created list

--- ptr11p4.c
+++ ptr11p4.c
@@ -8,7 +8,7 @@
 
 /* includes */
 
-
+#include <stdio.h>
 
 
 
@@ -25,6 +25,10 @@
 
 /* define structures */
 
+struct entry {
+    int value;
+    struct entry *next;
+};
 
 
 
@@ -34,11 +38,36 @@
 
 /* function declarations */
 
-
-
+void deleteEntry(struct entry *p2Entry);
+void printList(struct entry *p2List);
 
 
 int main(int argc, char *argv[]) {
+
+    struct entry listHead, n1, n2, n3, n4, n5, *theEntry;
+
+
+    /* init the values of the entries */
+
+    listHead.value = 0;
+    n1.value = 100;
+    n2.value = 200;
+    n3.value = 300;
+    n4.value = 400;
+    n5.value = 500;
+
+    /* hook the entries together to form the list */
+
+    listHead.next = &n1;
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = &n4;
+    n4.next = &n5;
+    n5.next = (struct entry *) 0;
+
+    /* print out the values of the list */
+
+    printList(&listHead);
 
 
 
@@ -53,10 +82,23 @@ int main(int argc, char *argv[]) {
 
 /* function definitions */
 
+void deleteEntry(struct entry *p2Entry) {
 
 
 
 
+
+
+
+
+}
+
+
+void printList(struct entry *p2List) {
+
+
+
+}
 
 
 
